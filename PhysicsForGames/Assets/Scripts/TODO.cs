b252de3fa3b911c3f08a2e2fe141584611e56d66
Sofi Wesson <w0lfskitten173@gmail.com>
2022-08-16 16:21:18 +1000

    TODO list

---
+++ PhysicsForGames/Assets/Scripts/TODO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    /*
+    can shoot wreaking ball and add force when shooting it
+    shooting chain does nothing
+    record distance dummy flys
+    reset button to reset dummy, wreaking ball, and battery
+    can't shoot triggers
+    */
+}
